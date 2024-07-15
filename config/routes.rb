2a6275cb034030ab2a96d696348029e0fe0ebe66
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-07-16 00:01:50 +0200

    Added nutrition api

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     get '/facts', to: 'facts#index'
     get '/dictionary', to: 'dictionary#index'
+    get '/nutrition', to: 'nutrition#index'
   end
 end
